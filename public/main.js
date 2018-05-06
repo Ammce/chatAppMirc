5271fb66d114781bcfbaca32d6a29e5ebc5489eb
Amel Muminovic <ammce@Amels-MacBook-Pro.local>
2018-05-06 19:16:40 +0200

    Uploading

---
+++ public/main.js
@@ -0,0 +1,34 @@
+$( document ).ready(function() {
+    const socket = io.connect('https://www.chatapp2k18.herokuapp.com/');
+
+    let user = $('#name');
+    let message = document.getElementById('message');
+    let btn = $('#send');
+    let output = document.getElementById('output');
+    let feedback = document.getElementById('feedback'); 
+
+//Emit events
+btn.click(() => {
+    socket.emit('chat', {
+        user: user.val(),
+        message: message.value
+    })
+})
+
+message.addEventListener('keypress', ()=> {
+    socket.emit('typing', {
+        user: user.val()
+    })
+})
+
+socket.on('chat', (data)=> {
+   output.innerHTML += '<p><span>' + data.user + ': ' + ' </span> ' + data.message + '</p>'
+   feedback.innerHTML = '';
+   message.innerHTML = '';
+})
+
+socket.on('typing', (data) => {
+    feedback.innerHTML = '<p>' + data.user + ' is typing a message...</p>'
+})
+
+});
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const socket = require('socket.io');
+
+
+
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+const server = app.listen(process.env.PORT || 3000, () => {
+    console.log("Server is upp and running");
+})
+
+//Socket setup
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+    console.log("Made socket connection", socket.id);
+
+    socket.on('chat', (data) => {
+        io.sockets.emit('chat', data);
+    })
+
+    socket.on('typing', (data) => {
+        socket.broadcast.emit('typing', data);
+    })
+});
